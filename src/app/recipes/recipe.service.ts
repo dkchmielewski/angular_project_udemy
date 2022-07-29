ee1792c004f7f82a340b1305ed338dfd2aa4ca6b
DAN-PC\Daniel <dkchmielewski@gmail.com>
2022-07-28 23:29:22 -0400

    added recipe detail

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
@@ -7,11 +8,23 @@ export class RecipeService {
 
     private recipes: Recipe[] = [
         new Recipe('Shrimp Broccoli', 'Delicious shrimp with healthy broccoli', 
-        'https://www.acouplecooks.com/wp-content/uploads/2020/12/Honey-Garlic-Shrimp-026.jpg'),
+        'https://www.acouplecooks.com/wp-content/uploads/2020/12/Honey-Garlic-Shrimp-026.jpg', 
+        [
+          new Ingredient('Shrimp', 5),
+          new Ingredient('Broccoli', 1)
+        ]), 
         new Recipe('Shrimp Cajun', 'Delicious shrimp with healthy broccoli', 
-        'https://www.acouplecooks.com/wp-content/uploads/2020/12/Honey-Garlic-Shrimp-026.jpg'),
+        'https://www.acouplecooks.com/wp-content/uploads/2020/12/Honey-Garlic-Shrimp-026.jpg', 
+        [
+          new Ingredient('Shrimp', 7),
+          new Ingredient('Cajun', 3)
+        ]),
         new Recipe('Shrimp Paella', 'Delicious shrimp with healthy broccoli', 
-        'https://www.acouplecooks.com/wp-content/uploads/2020/12/Honey-Garlic-Shrimp-026.jpg')
+        'https://www.acouplecooks.com/wp-content/uploads/2020/12/Honey-Garlic-Shrimp-026.jpg', 
+        [
+          new Ingredient('Shripm', 8),
+          new Ingredient('Mussels', 5)
+        ])
       ];
 
       getRecipes() {
