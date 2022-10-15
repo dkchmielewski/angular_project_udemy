bf19cd3ca2d6d7d1c93bf356ca755ee56564ec5b
dkchmielewski <66046504+dkchmielewski@users.noreply.github.com>
2022-10-15 00:35:03 -0400

    adding a delete and cancel functionality

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -54,4 +54,9 @@ export class RecipeService {
         this.recipes[index] = newRecipe;
         this.recipesChange.next(this.recipes.slice());
       }
+
+      deleteRecipe(index: number) {
+        this.recipes.splice(index, 1);
+        this.recipesChange.next(this.recipes.slice());
+      }
 }
