2d45187c9c2067c32c7830cc14f2068afc3ee026
dkchmielewski <66046504+dkchmielewski@users.noreply.github.com>
2022-10-31 00:14:38 -0400

    finished http module - not working but cool

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
@@ -21,7 +21,7 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http
+        return this.http
             .get<Recipe[]>(
                 'https://ng-course-recipe-book-a07a3-default-rtdb.firebaseio.com/recipes.json'
             )
@@ -33,11 +33,11 @@ export class DataStorageService {
                             ingredients: recipe.ingredients ? recipe.ingredients : []
                         };
                     });
+                }),
+                tap(recipes => {
+                    this.recipeService.setRecipes(recipes);
                 })
             )
-            .subscribe(recipes => {
-                this.recipeService.setRecipes(recipes);
-            });
     }
 
 }
